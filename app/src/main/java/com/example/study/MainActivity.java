2c0478196d968bc258945807b8b7d1ca020ae5d3
Kim daehyeon <daehyeon3351@gmail.com>
2023-01-15 05:47:32 +0900

    start

---
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.study;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText Type;
+    Button Done;
+    TextView Result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //R: 리소스를 관리하는 클래스, R.id: R 안의 리소스 id를 관리하는 클래스
+        Type = findViewById(R.id.Type);
+        Done = findViewById(R.id.Done);
+        Result = findViewById((R.id.Result));
+
+        Done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Result.setText(Type.getText());
+            }
+        });
+    }
+}
