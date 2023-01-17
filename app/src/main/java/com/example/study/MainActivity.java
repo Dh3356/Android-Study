229b7973cbaa4ff4eb0221f1a61d40c362381ea7
Kim daehyeon <daehyeon3351@gmail.com>
2023-01-18 01:23:39 +0900

    ListView

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -5,48 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
 {
-    private EditText et_user_id;
-    private Button btn_login;
-    private String text;
-
-
+    private ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //R: 리소스를 관리하는 클래스
-        //R.id: R 안의 리소스 id를 관리하는 클래스
-        et_user_id = findViewById(R.id.et_user_id);
-        btn_login = findViewById(R.id.btn_login);
+        list = (ListView) findViewById(R.id.list);
 
+        List<String> datalist = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, datalist);//Adapter에 datalist를 연결
+        list.setAdapter(adapter);//datalist가 연결된 Adapter와 ListView 연결
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                text = et_user_id.getText().toString();
-                if(text.length() > 0)//아이디를 검사하지 않을 것이므로 한 글자라도 입력하면 로그인한다
-                {
-                    Toast.makeText(getApplicationContext(), "login success", Toast.LENGTH_SHORT).show();//Toast Message를 짧게 띄운다
-                    Intent i = new Intent(MainActivity.this, SubActivity.class);//현재 Activity에서 SubActivity로 넘어가게
-                    i.putExtra("text", text);//데이터 송신(text 변수의 데이터를 value 값으로, "text"를 name 값으로 송신한다)
-                    startActivity(i);//새로운 Activity를 화면에 띄운다(SubActivity를 화면에 띄운다)
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "login fail", Toast.LENGTH_SHORT).show();//아이디를 입력하지 않았다면 로그임을 하지 않는다
-                }
-            }
-        });
+        //list에 data 추가
+        for(int i = 0; i<20; i++)
+        {
+            datalist.add("data "+Integer.toString(i));
+        }
+        adapter.notifyDataSetChanged();//list 갱신
+
     }
 }
